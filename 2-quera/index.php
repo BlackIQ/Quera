52629600bd92d2bb5f8598374aeafa10acc832e6
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-26 02:08:21 +0430

    Add simple PHP file

---
+++ 2-quera/index.php
@@ -0,0 +1,15 @@
+/*
+How much until Noroz
+By Amirhossein Mohammdi.
+
+Jul 26 2021 | 09:20
+
+https://github.com/BlackIQ/Quera/tree/main/2-quera
+
+Read README for more information.
+Under MIT License.
+*/
+
+<?php
+
+?>
