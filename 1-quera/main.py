cac128f384f6c3d7cdcbe91a89de7e7b5d52d75e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-05 18:48:39 +0430

    add sort for make app fast and work better

--- 1-quera/main.py
+++ 1-quera/main.py
@@ -12,13 +12,8 @@ Under MIT License.
 
 
 def game(number):
-    both = [int(a) for a in str(number)]
-    if both[0] > both[1]:
-        print(both[0] - both[1])
-    elif both[0] < both[1]:
-        print(both[1] - both[0])
-    else:
-        print(both[0] - both[1])
+    both = sorted([int(a) for a in str(number)], reverse=True)
+    return both[0] - both[1]
 
 
 game(int(input("Enter Number : ")))
